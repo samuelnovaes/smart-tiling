2607614db03d8914bba4ea1b460b49cf0f012a89
Samuel Novaes <samuel.novaes.96@gmail.com>
2025-11-08 06:08:56 -0300

    refactor: remove notification calls from window movement handlers and update method names

--- src/extension.ts
+++ src/extension.ts
@@ -1,32 +1,31 @@
 import { Extension } from '@girs/gnome-shell/extensions/extension';
-import * as Main from '@girs/gnome-shell/ui/main';
 import { Keybindings, Keys } from './keybinding';
 
 export default class SmartTilingExtension extends Extension {
   private keybindings?: Keybindings;
 
   handleMoveWindowRight() {
-    Main.notify('Move Window Right triggered');
+    //
   }
 
   handleMoveWindowLeft() {
-    Main.notify('Move Window Left triggered');
+    //
   }
 
-  handleMoveWindowTop() {
-    Main.notify('Move Window Top triggered');
+  handleMoveWindowUp() {
+    //
   }
 
-  handleMoveWindowBottom() {
-    Main.notify('Move Window Bottom triggered');
+  handleMoveWindowDown() {
+    //
   }
 
   override enable() {
     this.keybindings = new Keybindings(this.getSettings());
     this.keybindings.add(Keys.MOVE_WINDOW_RIGHT, this.handleMoveWindowRight);
     this.keybindings.add(Keys.MOVE_WINDOW_LEFT, this.handleMoveWindowLeft);
-    this.keybindings.add(Keys.MOVE_WINDOW_UP, this.handleMoveWindowTop);
-    this.keybindings.add(Keys.MOVE_WINDOW_DOWN, this.handleMoveWindowBottom);
+    this.keybindings.add(Keys.MOVE_WINDOW_UP, this.handleMoveWindowUp);
+    this.keybindings.add(Keys.MOVE_WINDOW_DOWN, this.handleMoveWindowDown);
   }
 
   override disable() {
