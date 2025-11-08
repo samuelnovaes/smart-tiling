aa033a1d0c9bd0f7cdc169a182366148b39674a9
Samuel Novaes <samuel.novaes.96@gmail.com>
2025-11-08 02:47:05 -0300

    refactor: update output directory to user-specific GNOME extensions path

--- esbuild.js
+++ esbuild.js
@@ -1,18 +1,21 @@
 import AdmZip from 'adm-zip';
 import { build } from 'esbuild';
-import { cpSync, readFileSync } from 'fs';
+import { cpSync, readFileSync, rmSync } from 'fs';
 import { resolve, dirname } from 'path';
+import os from 'os';
 
 const __dirname = dirname(new URL(import.meta.url).pathname);
 const resourcePath = resolve(__dirname, 'resources');
-const distPath = resolve(__dirname, 'dist');
 const metadata = JSON.parse(readFileSync(resolve(resourcePath, 'metadata.json'), 'utf8'));
+const distPath = resolve(os.homedir(), '.local/share/gnome-shell/extensions', metadata.uuid);
 const zipPath = resolve(__dirname, `${metadata.uuid}.zip`);
+
+rmSync(distPath, { recursive: true, force: true });
 
 await build({
   entryPoints: ['src/extension.ts'],
   bundle: true,
-  outdir: 'dist',
+  outdir: distPath,
   treeShaking: false,
   target: 'firefox78',
   platform: 'node',
