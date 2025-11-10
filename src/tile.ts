1ace26bc4b73154f63d48170f0ce28870ca9769f
Samuel Novaes <samuel.novaes.96@gmail.com>
2025-11-09 21:03:45 -0300

    feat: enhance tile positioning with caching and improved movement logic

--- src/tile.ts
+++ src/tile.ts
@@ -2,6 +2,8 @@ import * as Main from '@girs/gnome-shell/ui/main';
 import Meta from '@girs/meta-17';
 import Mtk from '@girs/mtk-17';
 
+const cache: Map<number, Mtk.Rectangle> = new Map();
+
 export enum Position {
   TOP = 'top',
   BOTTOM = 'bottom',
@@ -11,25 +13,41 @@ export enum Position {
   TOP_RIGHT = 'top-right',
   BOTTOM_LEFT = 'bottom-left',
   BOTTOM_RIGHT = 'bottom-right',
-  MAXIMIZED = 'maximized'
+  MAXIMIZED = 'maximized',
+  CENTER = 'center'
 }
 
 export class Tile {
   private window: Meta.Window;
   private bounds: Mtk.Rectangle;
   private screen: Mtk.Rectangle;
+  private id: number;
 
   constructor() {
     this.window = global.display.get_focus_window();
     this.bounds = this.window.get_frame_rect();
     const monitorIndex = this.window.get_monitor();
     this.screen = Main.layoutManager.getWorkAreaForMonitor(monitorIndex);
+    this.window.unmaximize();
+    this.id = this.window.get_id();
+    if (!cache.has(this.id)) {
+      cache.set(this.window.get_id(), this.bounds);
+      this.window.connect('unmanaged', () => {
+        cache.delete(this.id);
+      });
+      this.window.connect('position-changed', this.onChange.bind(this));
+      this.window.connect('size-changed', this.onChange.bind(this));
+    }
+  }
+
+  private onChange(window: Meta.Window) {
+    this.bounds = window.get_frame_rect();
+    if (this.position === Position.CENTER) {
+      cache.set(this.id, this.bounds);
+    }
   }
 
   get position(): Position | null {
-    if (this.window.is_maximized()) {
-      return Position.MAXIMIZED;
-    }
     const top = this.bounds.y <= this.screen.y;
     const bottom = (this.bounds.y + this.bounds.height) >= (this.screen.y + this.screen.height);
     const left = this.bounds.x <= this.screen.x;
@@ -58,16 +76,23 @@ export class Tile {
     if (!top && bottom && !left && right) {
       return Position.BOTTOM_RIGHT;
     }
-    return null;
+    if (top && bottom && left && right) {
+      return Position.MAXIMIZED;
+    }
+    return Position.CENTER;
   }
 
   move(position: Position) {
     if (position === Position.MAXIMIZED) {
-      return this.window.maximize();
+      return this.window.move_resize_frame(
+        true,
+        this.screen.x,
+        this.screen.y,
+        this.screen.width,
+        this.screen.height
+      );
     }
-    if (this.position === Position.MAXIMIZED) {
-      this.window.unmaximize();
-    }
+
     if (position === Position.TOP) {
       return this.window.move_resize_frame(
         true,
@@ -77,6 +102,7 @@ export class Tile {
         this.screen.height / 2
       );
     }
+
     if (position === Position.BOTTOM) {
       return this.window.move_resize_frame(
         true,
@@ -86,6 +112,7 @@ export class Tile {
         this.screen.height / 2
       );
     }
+
     if (position === Position.LEFT) {
       return this.window.move_resize_frame(
         true,
@@ -95,6 +122,7 @@ export class Tile {
         this.screen.height
       );
     }
+
     if (position === Position.RIGHT) {
       return this.window.move_resize_frame(
         true,
@@ -104,6 +132,7 @@ export class Tile {
         this.screen.height
       );
     }
+
     if (position === Position.TOP_LEFT) {
       return this.window.move_resize_frame(
         true,
@@ -113,6 +142,7 @@ export class Tile {
         this.screen.height / 2
       );
     }
+
     if (position === Position.TOP_RIGHT) {
       return this.window.move_resize_frame(
         true,
@@ -122,6 +152,7 @@ export class Tile {
         this.screen.height / 2
       );
     }
+
     if (position === Position.BOTTOM_LEFT) {
       return this.window.move_resize_frame(
         true,
@@ -131,6 +162,7 @@ export class Tile {
         this.screen.height / 2
       );
     }
+
     if (position === Position.BOTTOM_RIGHT) {
       return this.window.move_resize_frame(
         true,
@@ -139,6 +171,19 @@ export class Tile {
         this.screen.width / 2,
         this.screen.height / 2
       );
+    }
+
+    if (position === Position.CENTER) {
+      const cachedBounds = cache.get(this.id);
+      if (cachedBounds) {
+        return this.window.move_resize_frame(
+          true,
+          cachedBounds.x,
+          cachedBounds.y,
+          cachedBounds.width,
+          cachedBounds.height
+        );
+      }
     }
   }
 };
