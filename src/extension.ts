c5235bfecc4bff57a49104dc9f6aeeee3e7669ab
Samuel Novaes <samuel.novaes.96@gmail.com>
2025-11-09 21:03:07 -0300

    feat: improve window movement logic with enhanced position handling

--- src/extension.ts
+++ src/extension.ts
@@ -11,84 +11,107 @@ export default class SmartTilingExtension extends Extension {
 
   private moveWindowRight() {
     const tile = new Tile();
-    if(tile.position === Position.TOP_LEFT) {
+    if (tile.position === Position.TOP_LEFT) {
+      return tile.move(Position.TOP);
+    }
+    if (tile.position === Position.TOP) {
       return tile.move(Position.TOP_RIGHT);
     }
-    if(tile.position === Position.BOTTOM_LEFT) {
+    if (tile.position === Position.BOTTOM_LEFT) {
+      return tile.move(Position.BOTTOM);
+    }
+    if (tile.position === Position.BOTTOM) {
       return tile.move(Position.BOTTOM_RIGHT);
     }
-    if(tile.position === Position.TOP) {
-      return tile.move(Position.TOP_RIGHT);
+    if (tile.position === Position.LEFT) {
+      return tile.move(Position.CENTER);
     }
-    if(tile.position === Position.BOTTOM) {
-      return tile.move(Position.BOTTOM_RIGHT);
+    if (tile.position === Position.CENTER || tile.position === Position.MAXIMIZED) {
+      return tile.move(Position.RIGHT);
     }
-    return tile.move(Position.RIGHT);
   }
 
   private moveWindowLeft() {
     const tile = new Tile();
-    if(tile.position === Position.TOP_RIGHT) {
+    if (tile.position === Position.TOP_RIGHT) {
+      return tile.move(Position.TOP);
+    }
+    if (tile.position === Position.TOP) {
       return tile.move(Position.TOP_LEFT);
     }
-    if(tile.position === Position.BOTTOM_RIGHT) {
+    if (tile.position === Position.BOTTOM_RIGHT) {
+      return tile.move(Position.BOTTOM);
+    }
+    if (tile.position === Position.BOTTOM) {
       return tile.move(Position.BOTTOM_LEFT);
     }
-    if(tile.position === Position.TOP) {
-      return tile.move(Position.TOP_LEFT);
+    if (tile.position === Position.RIGHT) {
+      return tile.move(Position.CENTER);
     }
-    if(tile.position === Position.BOTTOM) {
-      return tile.move(Position.BOTTOM_LEFT);
+    if (tile.position === Position.CENTER || tile.position === Position.MAXIMIZED) {
+      return tile.move(Position.LEFT);
     }
-    return tile.move(Position.LEFT);
   }
 
   private moveWindowUp() {
     const tile = new Tile();
-    if(tile.position === Position.BOTTOM_LEFT) {
+    if (tile.position === Position.BOTTOM_LEFT) {
+      return tile.move(Position.LEFT);
+    }
+    if (tile.position === Position.LEFT) {
       return tile.move(Position.TOP_LEFT);
     }
-    if(tile.position === Position.BOTTOM_RIGHT) {
+    if (tile.position === Position.BOTTOM_RIGHT) {
+      return tile.move(Position.RIGHT);
+    }
+    if (tile.position === Position.RIGHT) {
       return tile.move(Position.TOP_RIGHT);
     }
-    if(tile.position === Position.LEFT) {
-      return tile.move(Position.TOP_LEFT);
-    }
-    if(tile.position === Position.RIGHT) {
-      return tile.move(Position.TOP_RIGHT);
-    }
-    if(tile.position === Position.TOP) {
+    if (tile.position === Position.TOP) {
       return tile.move(Position.MAXIMIZED);
     }
-    return tile.move(Position.TOP);
+    if (tile.position === Position.BOTTOM) {
+      return tile.move(Position.CENTER);
+    }
+    if (tile.position === Position.CENTER || tile.position === Position.MAXIMIZED) {
+      return tile.move(Position.TOP);
+    }
   }
 
   private moveWindowDown() {
     const tile = new Tile();
-    if(tile.position === Position.TOP_LEFT) {
+    if (tile.position === Position.TOP_LEFT) {
+      return tile.move(Position.LEFT);
+    }
+    if (tile.position === Position.LEFT) {
       return tile.move(Position.BOTTOM_LEFT);
     }
-    if(tile.position === Position.TOP_RIGHT) {
+    if (tile.position === Position.TOP_RIGHT) {
+      return tile.move(Position.RIGHT);
+    }
+    if (tile.position === Position.RIGHT) {
       return tile.move(Position.BOTTOM_RIGHT);
     }
-    if(tile.position === Position.LEFT) {
-      return tile.move(Position.BOTTOM_LEFT);
+    if (tile.position === Position.MAXIMIZED) {
+      return tile.move(Position.TOP);
     }
-    if(tile.position === Position.RIGHT) {
-      return tile.move(Position.BOTTOM_RIGHT);
+    if (tile.position === Position.TOP) {
+      return tile.move(Position.CENTER);
     }
-    return tile.move(Position.BOTTOM);
+    if (tile.position === Position.CENTER) {
+      return tile.move(Position.BOTTOM);
+    }
   }
 
   override enable() {
     this.gnomeKeybindingsSettings = this.getSettings('org.gnome.desktop.wm.keybindings');
     this.mutterKeybindingsSettings = this.getSettings('org.gnome.mutter.keybindings');
-    
+
     this.gnomeKeybindingsSettings.set_strv('maximize', []);
     this.gnomeKeybindingsSettings.set_strv('unmaximize', []);
     this.mutterKeybindingsSettings.set_strv('toggle-tiled-left', []);
     this.mutterKeybindingsSettings.set_strv('toggle-tiled-right', []);
-    
+
     this.keybindings = new Keybindings(this.getSettings());
 
     this.keybindings.add('move-window-right', this.moveWindowRight);
