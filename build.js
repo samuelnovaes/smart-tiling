9a0adcca1312e13758716db4a8fc255f4b8b914e
Samuel Novaes <samuel.novaes.96@gmail.com>
2025-11-12 11:07:14 -0300

    feat: implement build script to copy resources and create zip archive

--- build.js
+++ build.js
@@ -1,6 +1,5 @@
 import AdmZip from 'adm-zip';
-import { build } from 'esbuild';
-import { cpSync, readFileSync, rmSync } from 'fs';
+import { cpSync, readFileSync } from 'fs';
 import { resolve, dirname } from 'path';
 
 const __dirname = dirname(new URL(import.meta.url).pathname);
@@ -8,19 +7,6 @@ const resourcePath = resolve(__dirname, 'resources');
 const metadata = JSON.parse(readFileSync(resolve(resourcePath, 'metadata.json'), 'utf8'));
 const distPath = resolve(__dirname, 'dist');
 const zipPath = resolve(__dirname, `${metadata.uuid}.zip`);
-
-rmSync(distPath, { recursive: true, force: true });
-
-await build({
-  entryPoints: ['src/extension.ts'],
-  bundle: true,
-  outdir: distPath,
-  treeShaking: false,
-  target: 'ESNext',
-  platform: 'node',
-  format: 'esm',
-  external: ['gi://*', 'resource://*']
-});
 
 cpSync(resourcePath, distPath, { recursive: true });
 
